cce33db3f95c6f3c8537c782063b6ca9ad68f332
Dave Airlie <airlied@redhat.com>
2009-07-28 15:22:42 +1000

    vmware: change to using ABI version check

--- src/vmware.c
+++ src/vmware.c
@@ -18,7 +18,7 @@ char rcsId_vmware[] =
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
-#ifndef XSERVER_LIBPCIACCESS
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 6
 #include "xf86Resources.h"
 #endif
 
